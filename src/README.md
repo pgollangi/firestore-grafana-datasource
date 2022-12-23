d7eb8b0f1045b9ae7186ce4e3aacea32affe5301
Prasanna Kumar Gollangi <6123002+pgollangi@users.noreply.github.com>
2022-12-23 14:49:23 +0530

    Encrypted storage of service account

--- src/README.md
+++ src/README.md
@@ -9,12 +9,12 @@ Grafana Firestore Data Source Plugin enables integrating data on Firestore on to
 ## Features
 - [x] Use Google Firestore as data source for Grafana dashboards
 - [x] Configure Firestore data source with GCP `Project Id` and [`Service Account`](https://cloud.google.com/firestore/docs/security/iam) for authentication
-- [x] Query Firestore [collections](https://firebase.google.com/docs/firestore/data-model#collections)
-- [x] 
-- [ ] Query [Collection Groups](https://firebase.blog/posts/2019/06/understanding-collection-group-queries)
+- [x] Store `Service Account` data source configuration in Grafana encrypted storage [Secure JSON Data](https://grafana.com/docs/grafana/latest/developers/plugins/add-authentication-for-data-source-plugins/#encrypt-data-source-configuration)
+- [x] Query Firestore [collections](https://firebase.google.com/docs/firestore/data-model#collections) and path to collections
+- [x] Auto detect data types: `string`, `number`, `boolean`, `json`
 - [ ] Query selected fields from collection
 - [ ] Order query results
 - [ ] Limit query results
 - [ ] Count query results
-- [ ] Use [Secure JSON Data](https://grafana.com/docs/grafana/latest/developers/plugins/add-authentication-for-data-source-plugins/#encrypt-data-source-configuration) to save `Service Account` Firestore data source configuration
-- [ ] Consider [Grafafa global variables](https://grafana.com/docs/grafana/latest/dashboards/variables/add-template-variables/#global-variables) when querying resources.
+- [ ] Query [Collection Groups](https://firebase.blog/posts/2019/06/understanding-collection-group-queries)
+- [ ] Use of [Grafafa global variables](https://grafana.com/docs/grafana/latest/dashboards/variables/add-template-variables/#global-variables) in queries.
--- src/components/ConfigEditor.tsx
+++ src/components/ConfigEditor.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, PureComponent } from 'react';
-import { TextArea, InlineField, Input } from '@grafana/ui';
+import { InlineField, Input, SecretTextArea } from '@grafana/ui';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
-import { MyDataSourceOptions } from '../types';
+import { FirestoreSecureJsonData, MyDataSourceOptions } from '../types';
 
 interface Props extends DataSourcePluginOptionsEditorProps<MyDataSourceOptions> { }
 
@@ -12,7 +12,7 @@ export class ConfigEditor extends PureComponent<Props, State> {
     const { onOptionsChange, options } = this.props;
     const jsonData = {
       ...options.jsonData,
-      projectId: event.target.value,
+      projectId: event.target.value.trim(),
     };
     onOptionsChange({
       ...options, jsonData
@@ -22,42 +22,67 @@ export class ConfigEditor extends PureComponent<Props, State> {
   // Secure field (only sent to the backend)
   onServiceAccountChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     const { onOptionsChange, options } = this.props;
-    const jsonData = {
-      ...options.jsonData,
-      serviceAccount: event.target.value,
-    };
-
     onOptionsChange({
       ...options,
-      jsonData
+      secureJsonData: {
+        serviceAccount: event.target.value,
+      },
+    });
+  };
+
+  onResetServiceAccount = () => {
+    const { onOptionsChange, options } = this.props;
+    onOptionsChange({
+      ...options,
+      secureJsonFields: {
+        ...options.secureJsonFields,
+        serviceAccount: false,
+      },
+      secureJsonData: {
+        ...options.secureJsonData,
+        serviceAccount: '',
+      },
     });
   };
 
   render() {
     const { options } = this.props;
-    const { jsonData } = options;
-    // const secureJsonData = (options.secureJsonData || {}) as FirestoreSecureJsonData;
+    const { jsonData, secureJsonFields } = options;
+    const secureJsonData = (options.secureJsonData || {}) as FirestoreSecureJsonData;
 
     return (
       <div className="gf-form-group">
         <div className='gf-form-group'>
-            <InlineField label="Project Id" labelWidth={20} tooltip="Project Id of on GCP console to fetch firestore resources.">
-                <Input 
-                  onChange={this.onProjectIdChange}
-                  value={jsonData.projectId || ''}
-                  placeholder="GCP Project ID" 
-                  width={40}></Input>
-              </InlineField>
-              <InlineField label="Service Account" labelWidth={20} tooltip="Service Account having previliges to read all firestore resources. Least role expected is 'roles/datastore.viewer'">
-                <TextArea
-                  value={jsonData.serviceAccount || ''}
-                  label="Service Account"
-                  placeholder="Paste Service Account here"
-                  onChange={this.onServiceAccountChange}
-                  cols={80}
-                  rows={10}
-                />
-              </InlineField>
+          <InlineField required label="Project Id" labelWidth={20}
+            tooltip="Project Id of on GCP console to fetch firestore resources.">
+            <Input
+              onChange={this.onProjectIdChange}
+              value={jsonData.projectId || ''}
+              placeholder="Unique identifier for the GCP Project"
+              width={40}></Input>
+          </InlineField>
+          <InlineField required label="Service Account" labelWidth={20}
+            tooltip="Service Account having previliges to read all firestore resources. Least role expected is 'roles/datastore.viewer'">
+            <SecretTextArea
+              label="Service Account"
+              placeholder={`{
+      ...
+      "private_key_id": "...",
+      "private_key": "...",
+      "client_email": "...",
+      "client_id": "..."
+      ...
+}
+              `}
+
+              value={secureJsonData.serviceAccount || ''}
+              isConfigured={(secureJsonFields && secureJsonFields.serviceAccount) as boolean}
+              onReset={this.onResetServiceAccount}
+              onChange={this.onServiceAccountChange}
+              cols={80}
+              rows={10}
+            />
+          </InlineField>
         </div>
 
       </div>
